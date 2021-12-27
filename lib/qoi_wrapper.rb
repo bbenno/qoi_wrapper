2dc152271a5937c4acdd329253c15cb8d2d08cd0
Benno Bielmeier <github@bbenno.com>
2021-12-27 22:11:55 +0100

    Add interface methods

--- lib/qoi_wrapper.rb
+++ lib/qoi_wrapper.rb
@@ -5,5 +5,12 @@ require_relative "qoi_wrapper/qoi_wrapper"
 
 module QoiWrapper
   class Error < StandardError; end
-  # Your code goes here...
+
+  def write; end
+
+  def read; end
+
+  def encode; end
+
+  def decode; end
 end
--- test/test_qoi_wrapper.rb
+++ test/test_qoi_wrapper.rb
@@ -8,4 +8,20 @@ class TestQoiWrapper < Minitest::Test
   should "have a version number" do
     refute_nil ::QoiWrapper::VERSION
   end
+
+  should "respond to write" do
+    assert_respond_to(subject, :write)
+  end
+
+  should "respond to read" do
+    assert_respond_to(subject, :read)
+  end
+
+  should "respond to encode" do
+    assert_respond_to(subject, :encode)
+  end
+
+  should "respond to decode" do
+    assert_respond_to(subject, :decode)
+  end
 end
